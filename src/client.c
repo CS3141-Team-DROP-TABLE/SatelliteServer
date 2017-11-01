94a2c75987786901fb63b6abe50a868349ba8a0b
= <=>
2017-11-01 11:31:45 -0400

    add cert and test serverConnect

--- src/client.c
+++ src/client.c
@@ -13,20 +13,75 @@
 
 #include <ping.h>
 #include <linkedList.h>
+#include <serverConnect.h>
+
+
+#define CAFILE "intermediate.cert.pem"
+#define SERVERNAME "nhs_server_domain.com"
+
+
+void *ping_loop(void *argsin){
+  struct list *queue = ((struct ping_th_args*)argsin)->queue;
+  int sockfd = ((struct ping_th_args*)argsin)->sockfd;
+
+  char *pkt = create_packet();
+  set_src(pkt, ((struct ping_th_args*)argsin)->src);
+  
+  struct target *t;
+  while(1){
+    
+    t = (struct target*)ll_pq_dequeue(queue);
+    if(t == NULL){
+      usleep(750);
+    }else if(t->addr == 0){
+      free(t);
+      break;
+    }else{
+      printf("ping sent\n");
+      struct iphdr *ip = (struct iphdr*)pkt;
+      struct icmphdr *icmp = (struct icmphdr*)(pkt+sizeof(struct iphdr));
+      printf("ICMP: %d, Checksum: %04x\n", icmp->type, icmp->checksum);
+      send_ping(pkt, t->addr, sockfd);
+      free(t);
+    }
+  }
+
+}
+
+
+
+void *recv_loop(void *sockfd){
+  char buffer[1024];
+  int ret;
+  struct iphdr *ip = (struct iphdr*)buffer;
+  struct sockaddr_in rec_str;
+  while(1){
+    ret = recv(*(int*)sockfd, buffer, 1024, 0);
+    if(ret >= 0){
+      rec_str.sin_addr.s_addr = ip->saddr;
+      printf("ping received of size %d from %s\n\n", ret, inet_ntoa(rec_str.sin_addr));
+    }
+    usleep(75);
+
+  }
+
+}
+
+
+
 
 int main(){
 
-  
-  
-  int sockfd = create_raw_socket();
-  in_addr_t src = get_ip("wlp9s0\0\0", 6, sockfd);
-  
   
   
 
 
   struct list ping_queue;
   ll_initialize(&ping_queue);
+
+
+  int sockfd = create_raw_socket();
+  in_addr_t src = get_ip("wlp9s0\0\0", 6, sockfd);
   
   struct ping_th_args snd_args;
   snd_args.queue = &ping_queue;
@@ -46,6 +101,27 @@ int main(){
   struct target *t;
 
   tr.addr = dst.s_addr;
+
+  /*/
+
+  const char *server = "127.0.0.1\0";
+
+  struct connection conn;
+
+  conn.port = 5556;
+  conn.server = (char*)malloc(sizeof(char)*strlen(server));
+  memcpy(conn.server, server, strlen(server));
+ 
+  open_tcp(&conn);
+
+  connect_TLS(&conn, CAFILE, SERVERNAME, strlen(SERVERNAME));
+
+  tls_send(&conn, "hello\n\0\0", 7);
+  
+  char buff[2048];
+  tls_recv(&conn, buff, 2047);
+  printf("%s\n", buff);
+  //*/
   
   for(int i = 0; i < 100; i++){ 
     t = (struct target*)malloc(sizeof(struct target));
